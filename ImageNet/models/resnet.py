1771303f4429d044d07d25f3942eeaf4aa7d3a43
LazyDreamer157 <102004052+LazyDreamer157@users.noreply.github.com>
2024-02-07 17:20:51 +0900

    Update resnet.py

--- ImageNet/models/resnet.py
+++ ImageNet/models/resnet.py
@@ -21,22 +21,22 @@ model_urls = {
 }
 
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1, bit=32):
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1, bit=32, percent=100):
     """3x3 convolution with padding"""
     return QuantConv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                       padding=dilation, groups=groups, bias=False, dilation=dilation, bit=bit)
+                       padding=dilation, groups=groups, bias=False, dilation=dilation, bit=bit, percent=percent)
 
 
-def conv1x1(in_planes, out_planes, stride=1, bit=32):
+def conv1x1(in_planes, out_planes, stride=1, bit=32, percent=100):
     """1x1 convolution"""
-    return QuantConv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False, bit=bit)
+    return QuantConv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False, bit=bit, percent=percent)
 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None, bit=32):
+                 base_width=64, dilation=1, norm_layer=None, bit=32, percent=100):
         super(BasicBlock, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
@@ -45,13 +45,15 @@ class BasicBlock(nn.Module):
         if dilation > 1:
             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride, bit=bit)
+        self.perc=percent # std BOT perc%
+        self.conv1 = conv3x3(inplanes, planes, stride, bit=bit, percent=self.perc)
         self.bn1 = norm_layer(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes, bit=bit)
+        self.conv2 = conv3x3(planes, planes, bit=bit, percent=self.perc)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
+
 
     def forward(self, x):
         identity = x
@@ -76,17 +78,18 @@ class Bottleneck(nn.Module):
     expansion = 4
 
     def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-                 base_width=64, dilation=1, norm_layer=None, bit=32):
+                 base_width=64, dilation=1, norm_layer=None, bit=32, percent=100):
         super(Bottleneck, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
         width = int(planes * (base_width / 64.)) * groups
         # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, width, bit=bit)
+        self.perc=percent # std BOT perc%
+        self.conv1 = conv1x1(inplanes, width, bit=bit, percent=self.perc)
         self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation, bit=bit)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation, bit=bit, percent=self.perc)
         self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * self.expansion, bit=bit)
+        self.conv3 = conv1x1(width, planes * self.expansion, bit=bit, percent=self.perc)
         self.bn3 = norm_layer(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -119,7 +122,7 @@ class ResNet(nn.Module):
 
     def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
-                 norm_layer=None, bit=32):
+                 norm_layer=None, bit=32, **kwargs):
         super(ResNet, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
@@ -128,6 +131,8 @@ class ResNet(nn.Module):
 
         self.inplanes = 64
         self.dilation = 1
+        self.perc=kwargs['percent']
+
         if replace_stride_with_dilation is None:
             # each element in the tuple indicates if we should replace
             # the 2x2 stride with a dilated convolution instead
@@ -184,12 +189,12 @@ class ResNet(nn.Module):
 
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                            self.base_width, previous_dilation, norm_layer, bit=self.bit))
+                            self.base_width, previous_dilation, norm_layer, bit=self.bit, percent=self.perc))
         self.inplanes = planes * block.expansion
         for _ in range(1, blocks):
             layers.append(block(self.inplanes, planes, groups=self.groups,
                                 base_width=self.base_width, dilation=self.dilation,
-                                norm_layer=norm_layer, bit=self.bit))
+                                norm_layer=norm_layer, bit=self.bit, percent=self.perc))
 
         return nn.Sequential(*layers)
 
